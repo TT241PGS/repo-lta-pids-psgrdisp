ec3b7290f4a5a8ab29664717c5e78fc0e64f0b24
abhiche <abhimail1@gmail.com>
2021-01-29 16:49:31 +0530

    Add mrt icon to mrt status pane

--- lib/display_web/live/components/advisories_portrait_component.ex
+++ lib/display_web/live/components/advisories_portrait_component.ex
@@ -13,6 +13,9 @@ defmodule AdvisoriesPortrait do
     <div class="advisories-container">
       <div class="heading-wrapper">
         <div class="heading">
+          <svg if={{get_in(@message, [:type]) == "MRT"}} width="38.077" height="45" viewBox="0 0 38.077 45">
+            <path id="Icon_ionic-md-train" data-name="Icon ionic-md-train" d="M24.663,3.375c-9.519,0-19.038,1.179-19.038,9.476v22.5a8.31,8.31,0,0,0,8.329,8.286l-3.57,3.548v1.19H15.7l4.76-4.738h8.968l4.76,4.738h4.76V47.2l-3.57-3.548A8.317,8.317,0,0,0,43.7,35.362v-22.5C43.7,4.554,35.178,3.375,24.663,3.375ZM13.954,38.9a3.548,3.548,0,1,1,3.57-3.548A3.555,3.555,0,0,1,13.954,38.9Zm8.329-16.572h-11.9V12.851h11.9Zm4.76,0V12.851h11.9v9.476ZM35.373,38.9a3.548,3.548,0,1,1,3.57-3.548A3.555,3.555,0,0,1,35.373,38.9Z" transform="translate(-5.625 -3.375)" fill="#fff"/>
+          </svg>
           <h1 :if={{get_in(@message, [:type]) == "MRT"}}>MRT Status</h1>
           <h1 :if={{get_in(@message, [:type]) != "MRT"}}>Advisories</h1>
         </div>
