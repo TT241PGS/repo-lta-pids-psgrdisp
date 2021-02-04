479ccada842ae2af58409b6cd4fb844619c683bb
Aung Myo Oo <dev.aungmyooo2015@gmail.com>
2021-02-04 21:01:13 +0630

    mute remove in both PL (#35)

--- lib/display_web/live/components/multimedia_landscape_component.ex
+++ lib/display_web/live/components/multimedia_landscape_component.ex
@@ -9,7 +9,7 @@ defmodule MultimediaLandscape do
     ~H"""
     <div class="container two-columns ">
       <img  width="67%" :if={{@multimedia.type == "IMAGE"}} class="thick-borders" src="{{@multimedia.content}}" style="margin: 50px auto; display: block" alt="">
-      <video width="90%" :if={{@multimedia.type == "VIDEO"}} class="thick-borders" style="margin: 40px auto; display: block" autoplay muted loop>
+      <video width="90%" :if={{@multimedia.type == "VIDEO"}} class="thick-borders" style="margin: 40px auto; display: block" autoplay loop>
         <source src="{{@multimedia.content}}" type="video/mp4" />
       </video>
       <img  width="67%" :if={{@multimedia.type == "IMAGE SEQUENCE"}} class="thick-borders" src="{{@image_sequence_url}}" style="margin: 50px auto; display: block" alt="">
--- lib/display_web/live/components/multimedia_portrait_component.ex
+++ lib/display_web/live/components/multimedia_portrait_component.ex
@@ -9,7 +9,7 @@ defmodule MultimediaPortrait do
     ~H"""
     <div class="image-wrapper">
       <img :if={{@multimedia.type == "IMAGE"}} src="{{@multimedia.content}}" alt="">
-      <video width="100%" :if={{@multimedia.type == "VIDEO"}} autoplay muted loop>
+      <video width="100%" :if={{@multimedia.type == "VIDEO"}} autoplay loop>
         <source src="{{@multimedia.content}}" type="video/mp4" />
       </video>
       <img :if={{@multimedia.type == "IMAGE SEQUENCE"}} src="{{@image_sequence_url}}" alt="">
