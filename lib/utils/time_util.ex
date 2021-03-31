eb9732d039a574c2949835068dd22992ed5fb194
abhiche <abhimail1@gmail.com>
2021-03-31 18:10:09 +0530

    Show Arr only if ETA is 1 min or less

--- lib/utils/time_util.ex
+++ lib/utils/time_util.ex
@@ -144,7 +144,7 @@ defmodule Display.Utils.TimeUtil do
     eta = get_eta_in_seconds(time)
 
     cond do
-      eta <= 120 ->
+      eta <= 60 ->
         "Arr"
 
       eta >= 3600 ->
@@ -159,7 +159,7 @@ defmodule Display.Utils.TimeUtil do
 
   def format_min_to_eta_mins(eta) do
     cond do
-      eta <= 2 ->
+      eta <= 1 ->
         "Arr"
 
       eta > 60 ->
