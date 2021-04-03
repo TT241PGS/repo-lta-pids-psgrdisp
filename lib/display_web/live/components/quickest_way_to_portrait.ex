72c5914bb1d9e0661c711c0358060e3d99915f5d
abhiche <abhimail1@gmail.com>
2021-04-04 00:35:09 +0530

    Update QWT portrait style

--- lib/display_web/live/components/quickest_way_to_portrait.ex
+++ lib/display_web/live/components/quickest_way_to_portrait.ex
@@ -9,7 +9,7 @@ defmodule QuickestWayToPortrait do
     <div class="quickest-way">
       <div class="floating-heading">quickest way to</div>
       <div class="row">
-        <div class={{"row-odd": index in [0,1], "row-even": index in [2,3]}} :for={{ {service, index} <- Enum.with_index(@services) |> Enum.take(4) }}>
+        <div class={{"row", "row-odd": index in [0,2], "row-even": index in [1,3]}} :for={{ {service, index} <- Enum.with_index(@services) |> Enum.take(4) }}>
           <div class="group-info">
             <div class="left-info">
               <p>{{get_in(service, ["poi","poi_name"])}}</p>
@@ -18,12 +18,14 @@ defmodule QuickestWayToPortrait do
                   <img :for={{ poi <- get_in(service, ["poi", "pictograms"]) }} class="tag" src="{{poi}}" alt="">
                 </div>
               </div>
-              <div class="floating-arrow"><i class="fas fa-arrow-right"></i></div>
             </div>
-            <div class={{"right-info", "bus", alternate: get_in(service, ["type"]) == "alternate", main: get_in(service, ["type"]) == "main"}}>
-              <span :if={{get_in(service, ["type"]) == "main"}} class="number">{{service["service_no"]}}</span>
-              <span :if={{get_in(service, ["type"]) == "main"}} class="status">{{service["arriving_time_at_origin"]}}</span>
-              <span :if={{get_in(service, ["type"]) == "alternate"}} class="text">{{service["poi"]["poi_message"]}}</span>
+            <div class="flex items-center justify-between">
+              <div class={{"right-info", "bus", alternate: get_in(service, ["type"]) == "alternate", main: get_in(service, ["type"]) == "main"}}>
+                <div class="floating-arrow"><i class="fas fa-arrow-right"></i></div>
+                <span :if={{get_in(service, ["type"]) == "main"}} class="number">{{service["service_no"]}}</span>
+                <span :if={{get_in(service, ["type"]) == "main"}} class="status">{{service["arriving_time_at_origin"]}}</span>
+                <span :if={{get_in(service, ["type"]) == "alternate"}} class="text">{{service["poi"]["poi_message"]}}</span>
+              </div>
             </div>
           </div>
         </div>
