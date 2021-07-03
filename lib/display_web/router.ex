d04f0c550e860d09610f0729fcdba3befc577599
Thida Tun <sheetalsk97@gmail.com>
2021-07-03 14:59:15 +0630

    update router

--- lib/display_web/router.ex
+++ lib/display_web/router.ex
@@ -14,7 +14,7 @@ defmodule DisplayWeb.Router do
     plug :accepts, ["json"]
   end
 
-  forward "/alive", HealthCheckup, resp_body: "I'm up again!"
+  forward "/alive", HealthCheckup, resp_body: "I'm up!"
 
   scope "/", DisplayWeb do
     pipe_through :browser
