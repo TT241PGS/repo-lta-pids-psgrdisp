f64fa7d5198773e302e42d43739da89497d9c197
Hein <heinzeya@gmail.com>
2021-07-30 11:06:33 +0800

    fixed multimedia

--- lib/display_web/live/components/multimedia_landscape_component.ex
+++ lib/display_web/live/components/multimedia_landscape_component.ex
@@ -10,7 +10,7 @@ defmodule MultimediaLandscape do
     ~H"""
     <div class={{"container", "two-columns", "multimedia" , "one-pane": @onePane == true}}>
       <img :if={{@multimedia.type == "IMAGE"}} class="thick-borders media-item" src="{{@multimedia.content}}" alt="">
-  
+
       <video :if={{@multimedia.type == "VIDEO"}} oncanplaythrough="this.volume={{ @panel_audio_lvl }}" class="thick-borders media-item" autoplay loop>
         <source src="{{@multimedia.content}}" type="video/mp4" />
       </video>
